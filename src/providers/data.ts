f8f111540d1e97ed07fcbda002324d6b720eb4f1
Ayoub <ayoub18daoui@gmail.com>
2026-08-26 12:38:00 +0100

    implement rate limiting toast

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,22 @@
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 import { API_BASE_URL } from "./constants";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
+
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+  let message = "Request failed.";
+
+  try {
+    const payload = (await response.json()) as { message: string };
+
+    if (payload?.message) message = payload.message;
+  } catch {}
+
+  return {
+    message,
+    statusCode: response.status,
+  };
+};
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -30,6 +46,7 @@ const options: CreateDataProviderOptions = {
     },
 
     mapResponse: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
       const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
